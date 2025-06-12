f6964a5589147d6fb73f5d73974d1010ab262e17
cauafsantosdev <cauafsantosdev@gmail.com>
2025-06-11 23:52:25 -0300

    refactor: added attributes and changed parameters in Film class
    - Added id and studio attributes

--- scrapxd/models/film.py
+++ scrapxd/models/film.py
@@ -4,18 +4,20 @@ from typing import List, Dict
 
 class Film(BaseModel):
     slug: str
+    id: int
     title: str
     year: int
     runtime: int
-    director: str
-    genres: List[str] = []
-    nanogenres: List[str] = []
-    themes: List[str] = []
-    countries: List[str] = []
-    languages: List[str] = []
-    cast: List[str] = []
-    crew: Dict[str, str] = []
-    avg_rating: int
+    director: List[str]
+    genre: List[str] = []
+    nanogenres: List[str] | None = None
+    themes: List[str] | None = None
+    country: List[str] = []
+    language: List[str] = []
+    cast: Dict[str, str] = []
+    crew: Dict[str, List[str]] = []
+    studio: List[str] = None
+    avg_rating: float
     total_logs: int
 
     def __str__(self):
